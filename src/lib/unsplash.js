73bfeefa7617e8b6d7e91dab869ba79519da86e5
Hassan <35844352+HassanIT@users.noreply.github.com>
2024-07-30 10:14:28 -0400

    Update unsplash.js

--- src/lib/unsplash.js
+++ src/lib/unsplash.js
@@ -1,6 +1,6 @@
 export async function fetchPhotos(page = 1) {
   try {
-    const response = await fetch(`https://api.unsplash.com/photos?page=${page}&client_id=g8Z0gQAhbMs8GiDaEn22snot-G9tgDdrGtdB9fBIcSk`);
+    const response = await fetch(`https://api.unsplash.com/photos?page=${page}&client_id=${UNSPLASH_ACCESS_KEY}`);
     
     if (!response.ok) {
       throw new Error(`Error ${response.status}`);
